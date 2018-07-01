2bd8083798c9148fac2044ec60153c79bd3e809a
ZakiNata <zaki.natsheh@gmail.com>
2018-07-01 22:27:59 +0300

    removed un-needed code

--- KnapSackTest/Form1.cs
+++ KnapSackTest/Form1.cs
@@ -23,51 +23,9 @@ namespace KnapSackTest
 
         private void solveButton_Click(object sender, EventArgs e)
         {
-
-            //int[] value = { 20,30,25,6,10 };
-            string[] valueStr = valueBox.Text.Split(new string[] { "," }, StringSplitOptions.RemoveEmptyEntries);
-            int[] value = new int[valueStr.Length];
-            for (int i = 0; i < valueStr.Length; i++)
-            {
-                value[i] = Int32.Parse(valueStr[i]);
-            }
-
-            //int[] weight = { 55, 10, 20, 30,50 };
-            string[] weightStr = weightBox.Text.Split(new string[] { "," }, StringSplitOptions.RemoveEmptyEntries);
-            int[] weight = new int[weightStr.Length];
-            for (int i = 0; i < weightStr.Length; i++)
-            {
-                weight[i] = Int32.Parse(weightStr[i]);
-            }
-
-            int capacity = Int32.Parse(capacityBox.Text);
-            int itemsCount = weight.Length;
-
-            int result = SolveKnapSack(capacity, weight, value, itemsCount);
-
-            resultBox.Text = result.ToString();
-
+            
         }
 
-        public static int SolveKnapSack(int capacity, int[] weight, int[] value, int itemsCount)
-        {
-            int[,] K = new int[itemsCount + 1, capacity + 1];
-
-            for (int i = 0; i <= itemsCount; ++i)
-            {
-                for (int w = 0; w <= capacity; ++w)
-                {
-                    if (i == 0 || w == 0)
-                        K[i, w] = 0;
-                    else if (weight[i - 1] <= w)
-                        K[i, w] = Math.Max(value[i - 1] + K[i - 1, w - weight[i - 1]], K[i - 1, w]);
-                    else
-                        K[i, w] = K[i - 1, w];
-                }
-            }
-
-            return K[itemsCount, capacity];
-        }
 
         private void label1_Click(object sender, EventArgs e)
         {
