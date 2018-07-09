c88cd19327356951188207dc0c191ff8d58eb43e
ZakiNata <zaki.natsheh@gmail.com>
2018-07-10 01:42:39 +0300

    used a different method in the Linear programming

--- KnapSackTest/Form1.cs
+++ KnapSackTest/Form1.cs
@@ -6,6 +6,9 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using Microsoft.SolverFoundation.Common;
+using Microsoft.SolverFoundation.Solvers;
+
 
 namespace KnapSackTest
 {
@@ -79,20 +82,28 @@ namespace KnapSackTest
             Bag b = new Bag();
             
             b.setCapacity(capacity);
-            b.Calculate(knapsackItems);
-            //b.All(x => { Console.WriteLine(x); return true; });
-            //Console.WriteLine(b.Sum(x => x.Weight));
-            //Console.ReadKey();
 
-            LPBox.Text = b.Sum(x => x.Value).ToString();
+            //b.Calculate(knapsackItems);
 
             string itemsChosen = "";
-            List<Bag.Item>  choosenItems = b.ToList();
+            List<Bag.Item> choosenItems = b.ToList();
+            choosenItems = b.LinearProgramming(knapsackItems);
+
+            LPBox.Text = choosenItems.Sum(x => x.Value).ToString();
+            
             for (int k = 0; k < choosenItems.Count; k++)
             {
                 itemsChosen += choosenItems[k].Weight.ToString() + " ";
             }
+
             LPItemsBox.Text = itemsChosen;
+
+            //b.All(x => { Console.WriteLine(x); return true; });
+            //Console.WriteLine(b.Sum(x => x.Weight));
+            //Console.ReadKey();
+
+
+
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -383,6 +394,104 @@ namespace KnapSackTest
             return choosenItems;
         }
 
+        public List<Bag.Item> LinearProgramming(List<Item> items)
+        {
+            List<Bag.Item> choosenItems = new List<Bag.Item>();
+            int itemsCount = items.Count;
+            double capacity = MaxWeightAllowed;
+
+            SimplexSolver solver = new SimplexSolver();
+
+            //double[] estimatedProfitOfProjectX = new double[] { 1, 1.8, 1.6, 0.8, 1.4 };
+            //double[] capitalRequiredForProjectX = new double[] { 6, 12, 10, 4, 8 };
+            //double availableCapital = 20;
+            //int[] chooseProjectX = new int[5];
+
+            double[] values = new double[itemsCount];
+            double[] weights = new double[itemsCount];
+
+            for(int i=0;i<itemsCount;i++)
+            {
+                values[i] = items[i].Value;
+                weights[i] = items[i].Weight;
+            }
+
+            int[] chosenItems = new int[itemsCount];
+
+            //int profit;
+            //solver.AddRow("profit", out profit);
+            //solver.AddGoal(profit, 0, false);
+
+            int MaximumValue;
+            solver.AddRow("MaximumValue", out MaximumValue);
+            solver.AddGoal(MaximumValue, 0, false);
+
+            //int expenditure;
+            //solver.AddRow("expenditure", out expenditure);
+            //solver.SetBounds(expenditure, 0, availableCapital);
+
+            int MaximumWeight;
+            solver.AddRow("MaximumWeight", out MaximumWeight);
+            solver.SetBounds(MaximumWeight, 0, capacity);
+
+            //for (int i = 0; i < 5; i++)
+            //{
+            //    solver.AddVariable(string.Format("project{0}", i),
+            //                        out chooseProjectX[i]);
+            //    solver.SetBounds(chooseProjectX[i], 0, 1);
+            //    solver.SetIntegrality(chooseProjectX[i], true);
+            //    solver.SetCoefficient(profit, chooseProjectX[i],
+            //                           estimatedProfitOfProjectX[i]);
+            //    solver.SetCoefficient(expenditure, chooseProjectX[i],
+            //                           capitalRequiredForProjectX[i]);
+            //}
+
+            for (int i = 0; i < itemsCount; i++)
+            {
+                solver.AddVariable(string.Format("Item{0}", i),
+                                    out chosenItems[i]);
+                solver.SetBounds(chosenItems[i], 0, 1);
+                solver.SetIntegrality(chosenItems[i], true);
+                solver.SetCoefficient(MaximumValue, chosenItems[i],
+                                       values[i]);
+                solver.SetCoefficient(MaximumWeight, chosenItems[i],
+                                       weights[i]);
+            }
+
+            //SimplexSolverParams param = new SimplexSolverParams();
+            //param.MixedIntegerGenerateCuts = true;
+            //solver.Solve(param);
+
+            SimplexSolverParams param = new SimplexSolverParams();
+            param.MixedIntegerGenerateCuts = true;
+            solver.Solve(param);
+
+            //Console.WriteLine(solver.MipResult);
+            //for (int i = 0; i < 5; i++)
+            //{
+            //    Console.WriteLine("Project {0} is {1} selected.", i,
+            //                    solver.GetValue(chooseProjectX[i]) == 1 ? "" : "not ");
+            //}
+            //Console.WriteLine("The estimated total profit is: ${0} million.",
+            //                  (double)solver.GetValue(profit).ToDouble());
+            //Console.WriteLine("The total expenditure is: ${0} million.",
+            //                   solver.GetValue(expenditure).ToDouble());
+
+            //string result = "";
+            for (int i = 0; i < itemsCount; i++)
+            {
+                if(solver.GetValue(chosenItems[i]) == 1)
+                {
+                    //result += "Selected Item " + i + " ";
+                    choosenItems.Add(items[i]);
+                }
+            }
+            
+            //result += " value "+(double)solver.GetValue(MaximumValue).ToDouble()+ " weight "+ solver.GetValue(MaximumWeight).ToDouble();
+            
+
+            return choosenItems;
+        }
         public void Calculate(List<Item> items)
         {
             foreach (Item i in Sorte(items))
